a40c923deca3c13e904926fc83e151e817801f74
Niels <nhuisman@vtc.edu>
2017-05-03 12:45:51 -0400

    things with other things

--- RaspberryPi/practice/Bluetooth.py
+++ RaspberryPi/practice/Bluetooth.py
@@ -1,20 +1,48 @@
-# -*- coding: utf-8 -*-
+# file: rfcomm-client.py
+# auth: Albert Huang <albert@csail.mit.edu>
+# desc: simple demonstration of a client application that uses RFCOMM sockets
+#       intended for use with rfcomm-server
+#
+# $Id: rfcomm-client.py 424 2006-08-24 03:35:54Z albert $
 
-import time
-import bluetooth
-from wakeonlan import
+from bluetooth import *
+import sys
 
-phone = "ff:ff:ff:ff:ff:ff"
+if sys.version < '3':
+    input = raw_input
 
-def search():
-    devices = bluetooth.discover_devices(duration = 5, lookup_names = True)
-    return devices
+addr = None
 
+if len(sys.argv) < 2:
+    print("no device specified.  Searching all nearby bluetooth devices for")
+    print("the SampleServer service")
+else:
+    addr = sys.argv[1]
+    print("Searching for SampleServer on %s" % addr)
+
+# search for the SampleServer service
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+service_matches = find_service( uuid = uuid, address = addr )
+
+if len(service_matches) == 0:
+    print("couldn't find the SampleServer service =(")
+    sys.exit(0)
+
+first_match = service_matches[0]
+port = first_match["port"]
+name = first_match["name"]
+host = first_match["host"]
+
+print("connecting to \"%s\" on %s" % (name, host))
+
+# Create the client socket
+sock=BluetoothSocket( RFCOMM )
+sock.connect((host, port))
+
+print("connected.  type stuff")
 while True:
-    results = search()
+    data = input()
+    if len(data) == 0: break
+    sock.send(data)
 
-    for addr, name in results:
-        if addr == phone:
-            wol.send_magic_packet('ff:ff:ff:ff:ff:ff')
-
-        time.sleep(20)
+sock.close()
